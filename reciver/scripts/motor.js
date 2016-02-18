34e3ac51b896c43626710d2f11970c11692a03a4
Ryota Morita <ryota.morita.3.8@gmail.com>
2016-02-18 21:43:20 +0900

    操作を元に音がなるようにした

--- reciver/scripts/motor.js
+++ reciver/scripts/motor.js
@@ -1,4 +1,5 @@
 
+
 var CMD_to_MOTOR = {
     'FRONT':            motor__r10_l10,
     'FRONT_SLOW':       motor__r10_l10,
@@ -13,6 +14,18 @@ var CMD_to_MOTOR = {
     'LEFT_SMALL_MORE':  motor__r01_l10,
 }
 
+var DAIAL_MOTOR = {
+  '0000': 0, // 止まる
+  '1000': 1, // 左を軸に時計回り
+  '0100': 2, // 左を軸に反時計回り
+  '0010': 3, // 右を軸に反時計回り
+  '1010': 4, // 前に進む
+  '0110': 5, // その場で反時計回り
+  '0001': 6, // 右を軸に時計回り
+  '1001': 7, // その場で時計回り
+  '0101': 8, // 後ろに進む
+};
+
 function motor(direction) {
   CMD_to_MOTOR[direction]();
 }
@@ -20,45 +33,54 @@ function motor(direction) {
 // 止まる
 function motor__r00_l00() {
   console.log('motor__r00_l00');
+  playSound(DAIAL_MOTOR['0000']);
 };
 
 // 左を軸に時計回り
 function motor__r10_l00() {
   console.log('motor__r10_l00');
+  playSound(DAIAL_MOTOR['1000']);
 };
 
 // 左を軸に反時計回り
 function motor__r01_l00() {
   console.log('motor__r01_l00');
+  playSound(DAIAL_MOTOR['0100']);
 };
 
 // 右を軸に反時計回り
 function motor__r00_l10() {
   console.log('motor__r00_l10');
+  playSound(DAIAL_MOTOR['0010']);
 };
 
 // 前に進む
 function motor__r10_l10() {
   console.log('motor__r10_l10');
+  playSound(DAIAL_MOTOR['1010']);
 };
 
 // その場で反時計回り
 function motor__r01_l10() {
   console.log('motor__r01_l10');
+  playSound(DAIAL_MOTOR['0110']);
 };
 
 // 右を軸に時計回り
 function motor__r00_l01() {
   console.log('motor__r00_l01');
+  playSound(DAIAL_MOTOR['0001']);
 };
 
 // その場で時計回り
 function motor__r10_l01() {
   console.log('motor__r10_l01');
+  playSound(DAIAL_MOTOR['1001']);
 };
 
 // 後ろに進む
 function motor__r01_l01() {
   console.log('motor__r01_l01');
+  playSound(DAIAL_MOTOR['0101']);
 };
 
---
+++ reciver/scripts/sound.js
@@ -0,0 +1,47 @@
+
+
+// ogg形式のファイルが使えない場合にmp3形式のファイルを使う
+createjs.Sound.alternateExtensions = ["mp3"];
+// マニフェストの作成
+var manifest = [
+    {
+        src: "dtmf.ogg",
+        data: {
+            channels: 2,    // 同時に鳴らす音の数
+            audioSprite: [
+                {id: "0", startTime: 0,    duration: 100},
+                {id: "1", startTime: 200,  duration: 100},
+                {id: "2", startTime: 400,  duration: 100},
+                {id: "3", startTime: 600,  duration: 100},
+                {id: "4", startTime: 800,  duration: 100},
+                {id: "5", startTime: 1000, duration: 100},
+                {id: "6", startTime: 1200, duration: 100},
+                {id: "7", startTime: 1400, duration: 100},
+                {id: "8", startTime: 1600, duration: 100},
+                {id: "9", startTime: 1800, duration: 100},
+                {id: "-", startTime: 2000, duration: 100},
+                {id: "_", startTime: 2200, duration: 100},
+                {id: "a", startTime: 2400, duration: 100},
+                {id: "b", startTime: 2600, duration: 100},
+                {id: "c", startTime: 2800, duration: 100},
+                {id: "d", startTime: 3000, duration: 100}
+            ]
+        }
+    }
+];
+
+// マニフェストを登録
+createjs.Sound.registerSounds(manifest);
+// ファイル読み込み時の処理を設定
+//createjs.Sound.addEventListener("fileload", () => this.fileLoadHandler());
+
+/**
+ * 指定のIDの音を鳴らす
+ */
+function playSound(soundID) {
+    // パンを設定しているのは、Google Chromeで音が片方からしか聞こえないバグの対応のため
+    createjs.Sound.play(soundID, {pan: 0.01});
+}
+
+
+
